eaf567c92b049c8719a85dea4728196fbea0b536
2DU <min08101@naver.com>
2017-04-08 23:36:44 +0900

    코드 수정

--- fork.py
+++ fork.py
@@ -6,24 +6,40 @@ i = 0
 message = ''
 while True:
     print(i)
-    temp = requests.get('https://librewiki.net/index.php?title=%ED%8A%B9%EC%88%98:%EA%B8%B4%EB%AC%B8%EC%84%9C&limit=5000&offset=' + str(i * 5000))
+    
+    if(i == 0):
+        temp = requests.get('https://librewiki.net/wiki/특수:모든문서')
+    else:
+        temp = requests.get('https://librewiki.net/' + url)
+        
     if(temp):
         c = temp.text
-        if(re.search("명령에 대한 결과가 없습니다\.", c)):
-            break
+        
+        while True: 
+            m = re.search('<a href="\/wiki\/([^"]*)"(?: class="mw-redirect")? title="([^"]*)">([^<]*)<\/a>', c)
+            if(m):
+                g = m.groups()
+                message = message + g[2] + '\n'
+                
+                print(g[2])
+                
+                c = re.sub('<a href="\/wiki\/(?:[^"]*)"(?: class="mw-redirect")? title="([^"]*)">([^<]*)<\/a>', '', c, 1)
+            else:
+                break
+                
+        i = i + 1
+        
+        m = re.search('<a href="\/([^"]*)" title="특수:모든문서">다음 문서 \((?:[^<]*)<\/a>', c)
+        if(m):
+            g = m.groups()
+            url = re.sub('&amp;', '&', g[0], 1)
+            print(url)
         else:
-            while True: 
-                m = re.search('<a href="\/wiki\/(?:[^"]*)" title="(?:[^"]*)">([^<]*)<\/a>', c)
-                if(m):
-                    g = m.groups()
-                    message = message + g[0] + '\n'
-                    c = re.sub('<a href="\/wiki\/([^"]*)" title="([^"]*)">([^<]*)<\/a>', '', c, 1)
-                else:
-                    break
-            i = i + 1
+            break
     else:
         break
-message = re.sub('(?:개인정보 정책|리브레 위키 소개|면책 조항|이용약관|비밀번호를 잊으셨나요\?)\n', '', message)
+        
+message = re.sub("(개인정보 정책|리브레 위키 소개|면책 조항|이용약관|비밀번호를 잊으셨나요\?)\n", "", message)
 
 datafile = open(os.path.abspath(os.path.join('librewiki.txt')) ,'w', encoding="utf-8")
 datafile.write(message)
